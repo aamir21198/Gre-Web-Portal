562267bab14e264600909e5d34e5ea282cec819b
DhruvBhagadia <dhruvbhagadia@gmail.com>
2018-10-01 17:45:36 +0530

    Dynamic loading of Courses.. Fixed Read more of Universities.. hyperlink of universities offering courses left

--- php/Universities-Courses.php
+++ php/Universities-Courses.php
@@ -46,189 +46,147 @@
 					<button onclick="document.getElementById('Sign-up').style.display='block'" type="button" class="btn btn-secondary signup_button" 
 					data-toggle="dropdown">
 					Sign-up
-				</button>
+					</button>
+				</div>
+			</div>
+		</nav>
+	</nav>
+	<hr>	
+	<div class="card">
+		<div class="card-header">
+			<h3 class="heading my-2 mx-2">Universities</h3>
+		</div>
+		<div class="card-body">
+			<div class="container mx-2 my-2">
+				<table class="universities_ranking" border=1 frame=void rules=rows>
+					<tbody>
+						<tr class="table_header">
+							<th width="5%"> Rank </th>
+							<th width="40%" class="heading_name"> Name </th>
+							<th width="42%" class="heading_review"> Reviews </th>
+							<th width="30%"></th>
+						</tr>
+						<?php
+						$servername = "localhost";
+						$username = "root";
+						$password = "";
+						$dbname = "Gre_portal";
+
+						$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+						if (!$conn) {
+							die("Connection failed: " . mysqli_connect_error());
+						} 	
+							//echo "Connected successfully<br>";
+
+						$query = "SELECT * FROM Universities ORDER BY rank";
+						$result = mysqli_query($conn, $query);
+
+						if (mysqli_num_rows($result) > 0) {
+    					// output data of each row
+							$counter = 1;
+							while($row = mysqli_fetch_assoc($result)) {	
+								$first_break = strpos($row["review"], "\n");
+								$first_line = substr($row["review"], 0, $first_break);
+								$rem = substr($row["review"], $first_break);
+								echo 
+								" <tr class='row_$counter' style='overflow: visible;'>
+								<td>" . $row['rank'] . "</td>
+								<td>
+									<p><strong>" . $row['name'] . "</strong></p>
+									<p>" . $row['address'] . "</p>
+								</td>
+								<td>
+									<p class='review'></p>
+									$first_line	
+									<p id='more$counter' class='review' style='display:none;'>
+									$rem
+									</p>
+									<a href='javascript:showMore($counter)' id='link$counter'>  Read More >></a>
+								</td>
+								<td>
+								<a class='px-4' href='" . $row['website'] . "'>Learn more</a>
+								</td>
+								</tr>"
+								;
+								$counter = $counter + 1;
+							}
+						} else {
+							echo "0 results";
+						}
+						?>
+					</tbody>
+				</table>
 			</div>
 		</div>
-	</nav>
-</nav>
-<hr>	
-<div class="card">
-	<div class="card-header">
-		<h3 class="heading my-2 mx-2">Universities</h3>
 	</div>
-	<div class="card-body"> 
-		<div class="container mx-2 my-2">
-			<table class="universities_ranking" border=1 frame=void rules=rows>
-				<tbody> 
-					<tr class="table_header">
-						<th width="5%"> Rank </th>
-						<th width="40%" class="heading_name"> Name </th>
-						<th width="42%" class="heading_review"> Reviews </th>
-						<th width="30%"></th>
-					</tr>
-					
-					<?php
-					$servername = "localhost";
-					$username = "root";
-					$password = "";
-					$dbname = "Gre_portal";
-
-					$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-					if (!$conn) {
-						die("Connection failed: " . mysqli_connect_error());
-					} 
-						//echo "Connected successfully<br>";
-
-					$query = "SELECT * FROM Universities ORDER BY rank";
-					$result = mysqli_query($conn, $query);
-
-					if (mysqli_num_rows($result) > 0) {
-    				// output data of each row
-						$counter = 1;
-						while($row = mysqli_fetch_assoc($result)) {
-							$first_break = strrpos($row["review"], '\n');
-							$first_line = substr($row["review"], 0, $first_break);
-							$rem = substr($row["review"], $first_break);
-							echo 
-							" <tr class='row_$counter' style='overflow: visible;'>
-							<td>" . $row['rank'] . "</td>
-							<td>
-								<p><strong>" . $row['name'] . "</strong></p>
-								<p>" . $row['address'] . "</p>
-							</td>
-							<td>
-								<p class='review'></p>
-								$first_line	
-								<p id='more$counter' class='review' style='display:none;'>
-								$rem
-								</p>
-								<a href='javascript:showMore($counter)' id='link$counter'>  Read More >></a>
-							</td>
-							<td>
-							<a class='px-4' href='" . $row['website'] . "'>Learn more</a>
-							</td>
-							</tr>"
-							;
-							$counter = $counter + 1;
-						}
-					} else {
-						echo "0 results";
-					}
-					?>
-
-				</tbody>
-				  			</table>
-				  			</div>
-				  			</div>	
-				  			</div>
-				  			<hr>
-				  			<div class="card">
-				  			<div class="card-header">
-				  			<h3 class="heading my-2 mx-2">Courses</h3>
-				  			</div>
-				  			<div class="card-body"> 
-				  			<div class="container mx-2 my-2">
-				  			<table class="universities_ranking" border=1 frame=void rules=rows>
-				  			<tbody> 
-				  			<tr class="table_header">
+	<hr>
+	<div class="card">	
+		<div class="card-header">
+				<h3 class="heading my-2 mx-2">Courses</h3>
+		</div>
+		<div class="card-body">
+			<div class="container mx-2 my-2">	
+				<table class="courses_ranking" border=1 frame=void rules=rows>
+					<tbody>
+						<tr class="table_header">
 				  			<th width="5%"> Rank </th>
 				  			<th width="40%" class="heading_name"> Name </th>
 				  			<th width="42%" class="heading_review"> Overview of the Course </th>
-				  			<th width="30%"></th>
-				  			</tr>
-				  			<tr class="row_1" style="overflow: visible;">
-				  			<td>1.</td>
-				  			<td>
-				  			<p><strong>Computer Science</strong></p>
-				  			<hr>
-				  			<p>- Massachusetts</p>
-				  			<p>- Standford</p>
-				  			<p>- Ontario</p>
-				  			</td>
-				  			<td>
-				  			<p class="overview">
-				  			Computer science is the study of the theory, experimentation, and engineering that form the basis for the design and use of computers.
-				  			</p>
-				  			<p id="more_of_overview1" class="overview" style="display:none;">
-				  			It is the scientific and practical approach to computation and its applications and the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to, information. 
-				  			</p>
-				  			<a href="javascript:showMoreOverview(1);" id="link_overview1">  Read More >></a>
-				  			</td>
-				  			<td>
-				  			<a class="px-4" href="#">Learn more</a>
-				  			</td>
-				  			</tr>
-				  			<tr class="row_2" style="overflow: visible;">
-				  			<td>2.</td>
-				  			<td>
-				  			<p><strong>Computer Science</strong></p>
-				  			<hr>
-				  			<p>- Massachusetts</p>
-				  			<p>- Standford</p>
-				  			<p>- Ontario</p>
-				  			</td>
-				  			<td>
-				  			<p class="overview">
-				  			Computer science is the study of the theory, experimentation, and engineering that form the basis for the design and use of computers.
-				  			</p>
-				  			<p id="more_of_overview2" class="overview" style="display:none;">
-				  			It is the scientific and practical approach to computation and its applications and the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to, information. 
-				  			</p>
-				  			<a href="javascript:showMoreOverview(2);" id="link_overview2">  Read More >></a>
-				  			</td>
-				  			<td>
-				  			<a class="px-4" href="#">Learn more</a>
-				  			</td>
-				  			</tr>
-				  			<tr class="row_3" style="overflow: visible;">
-				  			<td>3.</td>
-				  			<td>
-				  			<p><strong>Computer Science</strong></p>
-				  			<hr>
-				  			<p>- Massachusetts</p>
-				  			<p>- Standford</p>
-				  			<p>- Ontario</p>
-				  			</td>
-				  			<td>
-				  			<p class="overview">
-				  			Computer science is the study of the theory, experimentation, and engineering that form the basis for the design and use of computers.
-				  			</p>
-				  			<p id="more_of_overview3" class="overview" style="display:none;">
-				  			It is the scientific and practical approach to computation and its applications and the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to, information. 
-				  			</p>
-				  			<a href="javascript:showMoreOverview(3);" id="link_overview3">  Read More >></a>
-				  			</td>
-				  			<td>
-				  			<a class="px-4" href="#">Learn more</a>
-				  			</td>
-				  			</tr>
-				  			<tr class="row_4" style="overflow: visible;">
-				  			<td>4.</td>
-				  			<td>
-				  			<p><strong>Computer Science</strong></p>
-				  			<hr>
-				  			<p>- Massachusetts</p>
-				  			<p>- Standford</p>
-				  			<p>- Ontario</p>
-				  			</td>
-				  			<td>
-				  			<p class="overview">
-				  			Computer science is the study of the theory, experimentation, and engineering that form the basis for the design and use of computers.
-				  			</p>
-				  			<p id="more_of_overview4" class="overview" style="display:none;">
-				  			It is the scientific and practical approach to computation and its applications and the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to, information. 
-				  			</p>
-				  			<a href="javascript:showMoreOverview(4);" id="link_overview4">  Read More >></a>
-				  			</td>
-				  			<td>
-				  			<a class="px-4" href="#">Learn more</a>
-				  			</td>
-				  			</tr>
-				  			</tbody>
-				  			</table>
-				  			</div>
-				  			</div>	
-				  			</div>
-				  			</body>
-				  			</html>
+				  		</tr>
+				  		<?php
+						$servername = "localhost";
+						$username = "root";
+						$password = "";
+						$dbname = "Gre_portal";
+
+						$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+						if (!$conn) {
+							die("Connection failed: " . mysqli_connect_error());
+						} 	
+							//echo "Connected successfully<br>";
+
+						$query = "SELECT * FROM Courses ORDER BY rank";
+						$result = mysqli_query($conn, $query);
+
+						if (mysqli_num_rows($result) > 0) {
+    					// output data of each row
+							$counter = 1;
+							while($row = mysqli_fetch_assoc($result)) {	
+								$first_break = strpos($row["overview"], ".");
+								$first_line = substr($row["overview"], 0, $first_break+1);
+								$rem = substr($row["overview"], $first_break+1);
+								echo 
+								" <tr class='row_$counter' style='overflow: visible;'>
+								<td>" . $row['rank'] . "</td>
+								<td>
+									<p><strong>" . $row['name'] . "</strong></p>
+									<hr>
+									<p><a href='#'>- Massachusetts</a></p>
+									<p><a href='#'>- Standford</a></p>
+									<p><a href='#'>- Ontario</a></p>
+								</td>
+								<td>
+									<p class='overview'></p>
+									$first_line	
+									<p id='more_of_overview$counter' class='overview' style='display:none;'>
+									$rem
+									</p>
+									<a href='javascript:showMoreOverview($counter)' id='link_overview$counter'>  Read More >></a>
+								</td>
+								</tr>"
+								;
+								$counter = $counter + 1;
+							}
+						} else {
+							echo "0 results";
+						}
+						?>
+					</tbody>
+				</table>						
+			</div>
+		</div>
+	</div>
+</body>
+</html>
